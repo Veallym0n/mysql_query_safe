dc7dbd0f17c3f8e1dd712b54e5e2e97afccc2fc2
Veallym0n <kevinlove1986@gmail.com>
2014-05-05 20:51:32 +0800

    写的更2了一些

--- test.php
+++ test.php
@@ -1,8 +1,9 @@
 <?php
-require_once(‘src/PHPSQLParser.php');
+require_once('PHPSQLParser.php');
 
-$DangerFunction = Array('benchmark','sleep','pg_sleep','concat_ws','system_user','load_file','database','current_user');
+$DangerFunction = Array('benchmark','sleep','pg_sleep','concat_ws','system_user','load_file','database','current_user','group_concat','rows_count');
 $DangerDB = Array('mysql','information_schema');
+$DangerSSV= Array('@@basedir','@@datadir','@@version_compile_os','@@max_allowed_packet','@@max_allowed_packet','@@skip_networking','@@table_type','@@character_set_database','@@log_error','@@tmpdir','@@Uptime');
 $score=0;
 
 function checksql($p,$insub=false){
@@ -16,7 +17,17 @@ function checksql($p,$insub=false){
             subchecksql_from($p[$keyname],$insub);
         }
 
-        if($keyname == 'WHERE'){ subchecksql_where($p[$keyname],$insub); }
+        if($keyname == 'WHERE'){ 
+            subchecksql_where($p[$keyname],$insub); 
+        }
+
+        if($keyname == 'ORDER'){
+            subchecksql_order($p[$keyname],$insub);
+        }
+
+        if($keyname == 'LIMIT'){
+            subchecksql_limit($p[$keyname],$insub);
+        }
 
         if(in_array($keyname,array("UNION","UNION ALL"))){
             foreach($p[$keyname] as $sub){
@@ -42,16 +53,9 @@ function splitFromtable($tablestr){
 
 
 function subchecksql_from($p,$insub=false){
-    global $DangerDB,$score;
     foreach($p as $sub){
         if($sub['expr_type']=='table'){
-            $dbmst = splitFromtable($sub['table']);
-            $dbname = $dbmst[0];
-            $tbname = $dbmst[1];
-            if(in_array($dbname,$DangerDB)){
-                echo "Deny ==> ",$sub['table'],"\n";
-                $score+=10;
-            }
+            is_dangerdbname($sub);
         }
     }
 }
@@ -61,10 +65,7 @@ function subchecksql_where($p,$insub=false){
     global $score;
     foreach($p as $sub){
         if($sub['expr_type']=='const'){
-            if(preg_match('/^0x[a-f0-9]+/i',$sub['base_expr'])){
-                echo "Deny ==> ",$sub['base_expr'],"\n";
-                $score+=2;
-            }
+            is_dangerconst($sub);
         }
         if($sub['expr_type']=='subquery'){
             if(gettype($sub['sub_tree']=='array')){
@@ -85,21 +86,91 @@ function subchecksql($p,$insub=false){
     global $DangerFunction,$score;
     foreach($p as $sub){
         if($sub['expr_type']=='const'){
-            if(preg_match('/^0x[a-f0-9]+/i',$sub['base_expr'])){
-                echo "Deny ==> ",$sub['base_expr'],"\n";
-                $score+=2;
-            }
+            is_dangerconst($sub);
+        }
+        if($sub['expr_type']=='session_variable'){
+            is_dangerssv($sub);
         }
         if($sub['expr_type']=='function'){
+            if($sub['base_expr']=='concat'){
+                specialfunc($sub);
+            }
             if(gettype($sub['sub_tree'])=='array'){
                 subchecksql($sub['sub_tree']);
             };
-            if(in_array($sub['base_expr'], $DangerFunction)){
-                echo "Deny ==> ",$sub['base_expr'],"\n";
-                $score+=2;
-            };
+            is_dangerFunc($sub);
         }
     }
+}
+
+
+function subchecksql_order($p,$insub=false){
+    global $score;
+    foreach($p as $sub){
+        is_dangerorder($sub);
+    }
+}
+
+
+function is_dangerFunc($sub){
+    global $DangerFunc,$score;
+    if(in_array($sub['base_expr'], $DangerFunction)){
+        echo "Deny(function) ==> ",$sub['base_expr'],"\n";
+        $score+=2;
+    }
+}
+
+function is_dangerdbname($sub){ 
+    global $DangerDB,$score;
+    $dbmst = splitFromtable($sub['table']);
+    $dbname = $dbmst[0];
+    $tbname = $dbmst[1];
+    if(in_array($dbname,$DangerDB)){
+        echo "Deny(dbname) ==> ",$sub['table'],"\n";
+        $score+=10;
+    }
+}
+
+function is_dangerssv($sub){
+    global $DangerSSV,$score;
+    if(in_array($sub['base_expr'],$DangerSSV)){
+        echo "Deny(function) ==> ",$sub['base_expr'],"\n";
+        $score+=4;
+    }
+}
+
+function is_dangerconst($sub){
+    global $score;
+    if(preg_match('/^0x[a-f0-9]+/i',$sub['base_expr'])){
+        echo "Deny(const) ==> ",$sub['base_expr'],"\n";
+        $score+=2;
+    }
+}
+
+function is_dangerorder($sub){
+    global $score;
+    if(!in_array($sub['expr_type'],array('pos','colref'))){
+        echo "Deny(order) ==> ",$sub['base_expr'],"\n";
+        $score+=2;
+    }
+}
+
+function is_dangerlimit($p){
+    global $score;
+    if(!($p['offset']=="" or is_numeric($p['offset']))){
+        echo "Deny(limit) ==> ",$sub['offset'],"\n";
+        $socre+=4;
+    }
+    if(!($p['rowcount']=="" or is_numeric($p['rowcount']))){
+        echo "Deny(limit) ==> ",$sub['offset'],"\n";
+        $socre+=4;
+    }
+    
+}
+
+
+function subchecksql_limit($p,$insub=false){
+    is_dangerlimit($p);
 }
 
 
@@ -118,12 +189,12 @@ function mysql_query_safe($sql){
     checksql($p);
     if($score<=4){
         echo "Valid SQL";
-        //mysql_query($sql)
+        //return mysql_query($sql)
     }else{
         echo "Danger! Score: ".$score."\n";
     }
 }
 
-mysql_query_safe("select owner_name,owner_desc from materials_owner where owner_id='14' and length((select distinct schema_name from `information_schema`.schemata limit 13,1))=62 and 1");
+mysql_query_safe("select * from mysql.user where id=1 union select 1,2,3,4,5,@@basedir");
 
 ?>
